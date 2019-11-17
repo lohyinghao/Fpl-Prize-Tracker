74cd199f8b23f5beb728517227621f89594b3dca
lohyinghao <lohyinghao91@gmail.com>
2019-11-17 17:40:37 +0800

    block cors from other server

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.ORIGIN || '*');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', process.env.ORIGIN || '*');
+//   next();
+// });
 
 app.get('/api/:url(*)', (req, res) => {
   request(req.params.url).pipe(res);
